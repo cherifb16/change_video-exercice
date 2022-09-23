89d19a37e2315e58fd75e36646b745792f9fa511
cherifb16 <cherifbobbyray@gmail.com>
2022-09-23 19:45:21 +0200

    first commit

---
+++ javascripts/ajax_sample.js
@@ -0,0 +1,34 @@
+let number = 0;
+let data = []; //--1
+const button = document.getElementById('btn');
+const titleArea = document.getElementById("title");
+const contentArea = document.getElementById("content");
+const videoArea = document.getElementById("video");
+
+function getData() {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4) {
+      if (request.status == 200) {
+        data = request.response
+      }
+    }
+  }
+  request.open("GET", "ajax.json");
+  request.responseType = "json"
+  request.send(null);
+}
+
+function changeVideo() {
+  if (data.length < 1) { //--2
+    getData();
+  }
+  button.addEventListener('click', e => {
+    titleArea.innerHTML = data[number].title;
+    contentArea.innerHTML = data[number].content;
+    videoArea.setAttribute("src", data[number].url);
+    number == 2 ? number = 0 : number++
+  })
+}
+
+window.onload = changeVideo;
